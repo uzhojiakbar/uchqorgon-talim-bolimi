cb1c4b813cbb57624d68291420f362c467b61166
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-07-18 03:03:07 +0500

    Warning

--- src/Components/Navbar/style.js
+++ src/Components/Navbar/style.js
@@ -3,8 +3,6 @@ import { styled } from "styled-components";
 const NavbarCon = styled.div`
     color: var(--white);
 `
-const Button = styled.div`
-    color: white;
-`
 
-export { NavbarCon, Button }
+
+export { NavbarCon }
