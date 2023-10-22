1f0395a14db96ac7190ac6b723033c1eed8c3a64
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-10-22 17:44:24 +0500

    maruzlar added

--- src/Pages/Maruza/index.jsx
+++ src/Pages/Maruza/index.jsx
@@ -7,7 +7,75 @@ const Maruzalar = () => {
             <PagesTitle title='Maruzalar' />
 
             <div className="main black paddingTop50">
+                <div className="bigTitle">
+                    XALQ TA`LIMI VAZIRI U.I.INOYATOVNING MA`RUZASI
+                </div>
+                <div className="fw700 marginTop15">
+                    Abdulla Avloniy nomidagi XTXQTMOMI da bo‘lib o‘tgan chet tili metodistlari yig‘ilishida Xalq ta`limi vaziri U.I.Inoyatovning ma`ruzasi   (07.02.2015)
+                </div>
+                <div className="head texts">
+                    <div>
+                        Hurmatli tuman(shahar), viloyat xalq ta(limi boshqaruv idoralari chet tili metodistlari, viloyat malaka oshirish institutining chet tili o‘qituvchilari!
+                    </div>
 
+                    <div>
+                        Men bugun Sizlar bilan Prezidentimizning 2012-yil 10-dekabrdagi "Chet tillarni o‘rganish tizimini yanada takomillashtirish chora-tadbirlari to‘g‘risida"gi qarorida belgilangan vazifalar ijrosi to‘g‘risida gaplashmoqchiman.
+                    </div>
+
+                    <div>
+                        Mazkur qarorda bilasiz-ki, o‘sib kelayotgan yosh avlodni chet tillarga o‘qitish, shu tillarda erkin so‘zlasha oladigan mutaxassislar tayyorlash tizimini takomillashtirish,ularning jahon sivilizatsiyasi yutuqlari va axborot resurslaridan keng ko‘lamda foydalanishlari, xalqaro hamkorlik va muloqotni rivojlantirishlari uchun imkoniyatlar yaratish bosh maqsad etib belgilangan.
+
+                    </div>
+
+                    <div>
+                        Ushbu maqsadda Uzluksiz ta(lim tizimi uchun chet tillaridan Davlat ta(lim standartlari yaratilib, tasdiqlandi va 2013-2014-o‘quv yilidan maktablarda chet tillari 1-sinfdan boshlab o‘qitila boshlandi.
+                    </div>
+
+                    <div>
+                        Mazkur DTS va o‘quv dasturi asosida 1- va 2-sinflar uchun darslik majmualari (darslik, o‘qituvchi kitobi, mashq daftari va DVD disk) yaratilib chop etildi va o‘quvchilarga bepul yetkazib berildi.
+                    </div>
+
+                    <div>
+                        Bugungi kunda chet tillaridan 3-sinflar uchun yaratilgan darslik majmualari tajriba-sinovdan o‘tkazilmoqda. Amaldagi 5-9-sinflar darsliklari yangi Davlat ta(lim standartlari asosida qayta ishlanib, chop etilib, ular ham bepul joylarga yetkazilmoqda.
+                    </div>
+                    <div>
+                        Boshlang‘ich sinflarda chet tillarini o‘qitish uchun 17222 ta chet tili xonalari tayyorlandi va ularning barchasi DVD-magnitola, o‘quv-ko‘rgazmali qurollar, lug‘at va adabiyotlar bilan ta(minlandi. O‘tgan yili 6650 ta boshlang‘ich sinflarga noutbuk kompyuterlar, proektor va boshqa texnik vositalar va 1500 ta 5-9-sinflar uchun jihozlar yetkazilgan bo‘lsa, bu yil 6624 ta chet tili xonalariga texnik vositalar va jihozlar yetkazib beriladi.
+                    </div>
+                    <div>
+                        2015-yil 1-martdan boshlab, amaldagi malaka oshirish tizimiga qo‘shimcha ravishda, barcha tumanlarda, o‘qituvchilarning o‘z hududlarida haftada bir martalik uzluksiz malaka oshirish kurslari tashkil etish choralari ko‘rildi. Doimiy ishlaydigan ushbu kurslarga bir yilda 20 ming nafar o‘qituvchilar qamrab olinadi.
+                    </div>
+
+                    <div>
+                        Ushbu kurslarini sifatli tashkil etish uchun barcha tumanlardan eng yuqori malakaga ega bo‘lgan 732 nafar tajribali o‘qituvchilar saralab olinib, joriy yil 26- yanvar – 6-fevral kunlari shu institutda 36 soatlik maxsus kursda trener sifatida tayyorlandi.
+                    </div>
+
+                    <div>
+                        Ushbu trenerlar eng zamonaviy texnik vositalar bilan jihozlangan 552 ta tayanch maktablarda haftada 1 kun 6 soatdan ingliz tili o‘qituvchilarni o‘qitadi. Unda o‘qituvchining og‘zaki nutq ko‘nikmalarini va yosh bolalar bilan ishlash metodikasini rivojlantirish, ularni chet tillaridan xalqaro darajadagi talablar asosida tayyorlangan testlarga tayyorlashga e(tibor qaratiladi.
+                    </div>
+
+                    <div>
+                        Ushbu malaka oshirish kurslari uchun o‘quv dasturlari, qo‘llanma va tarqatma materiallar O‘zDJTU qoshidagi inovatsion markaz tomonidan tayyorlanmoqda.  Ushbu jarayon hududlarda Xalq ta(limining hududiy malaka oshirish institutlari tomonidan muvofiqlashtirib boriladi. Erishiladigan natijalar asosida o‘quv dasturlari takomillashtiriladi va shu tariqa chet tili o‘qituvchilarining sifat tarkibi yaxshilanib boradi.
+                    </div>
+
+                    <div>
+                        Yana bir yangilik, har bir maktab va tumanda ingliz tili o‘qituvchilari orasida "Yilning eng yaxshi ingliz tili o‘qituvchisi" ko‘rik-tanlovini tashkil etish va o‘tkazish bo‘yicha Nizom ishlab chiqildi. Unga muvofiq chet tili o‘qituvchilari moddiy hamda ma(naviy rag‘batlantiriladi. Viloyat bosqichi g‘oliblari Yevropadagi til o‘rgatish markazlariga tajriba almashish xizmat safarlariga yuboriladi.
+                    </div>
+
+                    <div>
+                        Bularni o‘z vaqtida va sifatli tashkil etish hammamizga, ayniqsa shu zalda o‘tirgan viloyat, tuman chet tili metodistlariga va viloyat malaka oshirish instituti chet tili bo‘yicha mas(ullariga alohhida bog‘liq. Jumladan, Kurslarga tinglovchilarni qabul qilish va rasmiylashtirish tuman ta(lim bo‘limlari ro‘yxatlari asosida tegishli hududiy institut rektorining buyrug‘i bilan  amalga oshiriladi.
+                    </div>
+
+                    <div>
+                        Mashg‘ulotlarni o‘tkazish sifatining nazorati, tegishli hududiy institutlar bilan birgalikda, hududiy ta(limni boshqarish organlari tomonidan amalga oshiriladi, tuman ta(lim bo‘limlari esa tinglovchilarning har haftalik kurslardagi davomatini ta(minlaydilar. O‘tkazilgan darslar va tinglovchilarning davomati qat(iy hisobda turuvchi hujjat(jurnal)larda trenerlar tomonidan qayd etiladi, hududiy institutlar bilan birgalikda tuman ta(lim bo‘limlari tomonidan nazorat qilinadi.
+                    </div>
+
+                    <div>
+                        Shu kabi vazifalar bugun sizlarga tushuntiriladi. Tushlik tayyorlangan, keyin ingliz tilidan bilim darajangiz va metodistilik mahoratingiz sinovdan o‘tkaziladi va o‘z kasbiga loyiq deb hisoblanganlar, O‘zDJTU qoshidagi innovatsion markazda 144 soat hajmida malakasini oshiradi.
+                    </div>
+                    <div>
+                        Men Sizlarning ushbu mas(uliyatli topshiriqlarni uddalashlaringizga ishonaman.
+                    </div>
+                </div>
             </div>
         </div>
     )
